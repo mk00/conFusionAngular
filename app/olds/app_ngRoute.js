4c365874791c402fd12c6944733dca3d5fdbbcac
Daniel Rodriguez <djrr19@gmail.com>
2017-10-31 12:11:23 -0400

    Primer commit desde el equipo de la oficina, carpeta conFusion Angular js

---
+++ app/olds/app_ngRoute.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('confusionApp', ['ngRoute'])
+    
+    
+.config(function($routeProvider){
+        $routeProvider
+        
+        .when('/contactus',{
+            templateUrl : 'contactus.html',
+            controller : 'ContactController'
+        })
+        .when('/menu',{
+            templateUrl : 'menu.html',
+            controller : 'MenuController'
+        })
+        .when('/menu/:id',{
+            templateUrl : 'dishdetail.html',
+            controller : 'DishDetailController'
+        })
+        .otherwise('/contactus');
+    })
+;
