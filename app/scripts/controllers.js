b197c09d762c59aeb27de005f26ef188236e8689
Daniel Rodriguez <djrr19@gmail.com>
2017-11-01 16:03:34 -0400

    Instale bower para poder descargar el paquete ngResource y configrar los archivos de este commit para que trabajen con REST, en vez de http, los mismos fueron modificados para que funcionaran, todavia no manejan errores de comunicacion

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,21 +7,11 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = false;
+            $scope.showMenu = true;
             $scope.message = "Loading...";
-            $scope.dishes = {};
 
-            menuFactory.getDishes()
-                .then(
-                        function(response){
-                            $scope.dishes = response.data;
-                            $scope.showMenu = true;
-                        },
-                        function(response){
-                            $scope.message = "Error: "+response.status+" "+response.statusText;
-                        }
-                    );
-                        
+            $scope.dishes = menuFactory.getDishes().query();
+
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
                 
@@ -80,23 +70,10 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-                $scope.dish = {};
-                $scope.showDish = false;
+                
+                $scope.showDish = true;
                 $scope.message="Loading...";
-
-                    menuFactory.getDish(parseInt($stateParams.id,10))
-                    .then(
-                        function(response){
-                            $scope.dish = response.data;
-                            $scope.showDish = true;
-                            },
-                            function(response){
-                            $scope.message = "Error: "+response.status+" "+response.statusText;
-                            }
-                        );
-            
-           // $scope.dish = dish;
-            
+                $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
@@ -124,21 +101,13 @@ angular.module('confusionApp')
         }])
 
         .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
-                $scope.dish = {};
-                $scope.showDish = false;
+             
+                $scope.showDish = true;
                 $scope.message="Loading...";
-                    menuFactory.getDish(0)
-                    .then(
-                        function(response){
-                            $scope.dish = response.data;
-                            $scope.showDish = true;
-                            },
-                            function(response){
-                            $scope.message = "Error: "+response.status+" "+response.statusText;
-                            }
-                        );   
+                $scope.dish = menuFactory.getDishes().get({id:0});    
+                   
            $scope.leader = corporateFactory.getLeader(3);
-            //$scope.dish = menuFactory.getDish(0);
+          
             $scope.promotion = menuFactory.getPromotion(0);
         
         }])
