33be9be06c18db802ea232c0adbe5cf1a21e3a7f
Daniel Rodriguez <djrr19@gmail.com>
2017-11-02 14:26:55 -0400

    Se modificaron estos archivos para que puedan manejar errores de conectividad con el server side, ademas se modifico el controlador de dishcommentdetail para que pudiera hacer push en la base de datos de prueba db.json de los comentarios de los dish

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,10 +7,17 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = true;
+            $scope.showMenu = false;
             $scope.message = "Loading...";
 
-            $scope.dishes = menuFactory.getDishes().query();
+            $scope.dishes = menuFactory.getDishes().query(
+                    function(response){
+                        $scope.dishes = response;
+                        $scope.showMenu = true;
+                    },
+                    function(response){
+                        $scope.message = "Error: "+ response.status +" "+ response.statusText;
+            });
 
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -71,12 +78,20 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
                 
-                $scope.showDish = true;
+                $scope.showDish = false;
                 $scope.message="Loading...";
-                $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+                $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+                    .$promise.then(
+                        function(response){
+                            $scope.dish = response;
+                            $scope.showDish = true;
+                        },
+                        function(response){
+                            $scope.message = "Error: "+ response.status +" "+response.statusText;
+                        });
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope','menuFactory', function($scope,menuFactory) {
             
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             
@@ -86,7 +101,7 @@ angular.module('confusionApp')
                 console.log($scope.mycomment);
                 
                 $scope.dish.comments.push($scope.mycomment);
-                
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
                 $scope.commentForm.$setPristine();
                 
                 $scope.mycomment = {rating:5, comment:"", author:"", date:""};
@@ -102,9 +117,18 @@ angular.module('confusionApp')
 
         .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
              
-                $scope.showDish = true;
+                $scope.showDish = false;
                 $scope.message="Loading...";
-                $scope.dish = menuFactory.getDishes().get({id:0});    
+                $scope.dish = menuFactory.getDishes().get({id:0})
+                    .$promise.then(
+                        function(response){
+                            $scope.dish = response;
+                            $scope.showDish = true;
+                        },
+                        function(response){
+                            $scope.message = "Error: "+ response.status +" "+ response.statusText;
+                        }
+                    );    
                    
            $scope.leader = corporateFactory.getLeader(3);
           
